e57041f16ef48f1ca9a2bb9eead14cb208a6cb27
John <40970351+funnyjerry@users.noreply.github.com>
2019-10-31 02:40:12 +0800

    add project model

--- gkzRestApi/management/models.py
+++ gkzRestApi/management/models.py
@@ -1,19 +1,28 @@
 from django.db import models
 from django.core.validators import RegexValidator, MaxValueValidator, MinValueValidator
+from .utils import Projects
 
 class Customer(models.Model):
     name = models.CharField(max_length=70, blank=False, unique=True,)
     age = models.IntegerField(blank=False, default=0, validators=[MaxValueValidator(99), MinValueValidator(1)])
     active = models.BooleanField(default=False)
 
-class Singer(models.Model):
-    email        = models.CharField(max_length=255, verbose_name='Email Address', unique=True)
-    first_name   = models.CharField(max_length=255, verbose_name='First name',    blank=True )
-    last_name    = models.CharField(max_length=255, verbose_name='Last name',     blank=True )
-    company_name = models.CharField(max_length=255, verbose_name='Company name',  blank=True )
-    phone_number = models.CharField(max_length=17,  verbose_name='Phone number',  blank=True, validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: " "'+491725784200'. Up to 15 digits allowed.")],)
-    fax_number   = models.CharField(max_length=17,  verbose_name='Fax number',    blank=True, validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: " "'+491725784200'. Up to 15 digits allowed.")],)
-    tax_number   = models.CharField(max_length=255, verbose_name='Tax number',    blank=True )
-    is_active    = models.BooleanField(default=True)
-    is_admin     = models.BooleanField(default=False)
-    is_staff     = models.BooleanField(default=False)
+class Project(models.Model):
+    project_name            = models.CharField(max_length=64, blank=False, verbose_name='Project Name')
+    project_short_name      = models.CharField(max_length=5, blank=False, verbose_name='Project Short Name' )
+    project_id              = models.CharField(max_length=5, blank=False, verbose_name='Project ID')
+    project_code            = models.CharField(max_length=5, unique=True, verbose_name='Project Code')
+    project_register_type   = models.CharField(max_length=5, blank=False, verbose_name='Project Register Type', choices=Projects.register_types)
+    project_start_date      = models.DateTimeField(auto_now_add=True, blank=True, verbose_name='Project Start Date')
+    completion_date         = models.DateTimeField(null=True, blank=True, verbose_name='Completion Date')
+    project_type            = models.IntegerField(default=1, blank=False, verbose_name='Project Type', choices=Projects.types)
+    default_address_level   = models.IntegerField(default=1, blank=False, verbose_name='Project Address Level', choices=Projects.address_levels)
+    project_suburb          = models.CharField(max_length=5, blank=True, verbose_name='Project Suburb')
+    project_state           = models.CharField(max_length=5, blank=True, verbose_name='Project State')
+    project_post_code       = models.CharField(max_length=5, blank=True, verbose_name='Project Post Code')
+    project_country         = models.CharField(max_length=3, blank=False, verbose_name='Project Country', choices=Projects.countries)
+    address1                = models.CharField(max_length=64, blank=True, verbose_name='Address1')
+    address2                = models.CharField(max_length=64, blank=True, verbose_name='Address2')
+    address3                = models.CharField(max_length=64, blank=True, verbose_name='Address3')
+    project_description     = models.TextField(blank=False, verbose_name='Project Description' )
+
